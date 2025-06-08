8d3b04a49416d9648cee8756c970bd346226fa0d
Ravikumar S <ravi2422000@gmail.com>
2025-06-08 11:15:49 +0530

    Added the Amazon Search Case

--- tests/APIautomation.spec.ts
+++ tests/APIautomation.spec.ts
@@ -6,3 +6,19 @@ test('Amazon Home Page ', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Amazon/);
 });  
+
+test('Amazon Home Page Search ', async ({ page }) => {
+
+  await page.goto('https://www.amazon.in/');
+
+  await page.locator("//*[@id='nav-search']").waitFor();
+
+  await page.locator("//*[@id='nav-search']").click();
+
+  await page.locator("//*[@id='twotabsearchtextbox']").fill('LaptopStand');
+
+  await page.keyboard.press('Enter');
+
+  await page.waitForTimeout(5000);
+
+});  
